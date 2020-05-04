559ff5114373e933cc3eeff7901c8a7662997408
drdong <szdongofficial@gmail.com>
2020-05-05 01:31:14 +0900

    Bug fix.

--- daily.py
+++ daily.py
@@ -1,6 +1,7 @@
 import ccxt
 import requests
 import json
+import time
 
 api_key = "YOUR_BINANCE_API_KEY"
 api_secret = "YOUR_BINANCE_API_SECRET"
@@ -37,38 +38,34 @@ def lambda_handler(event, context):
 
     # 毎日購入する量(BTC)
     order_amount = round(order_size / btc_price, digit) + 0.000002
+    check = False
 
     # 成行注文
-    try:
-        market_buy_order = binance.create_market_buy_order("BTC/USDT", order_amount)
-        line_notify(str(json.dumps(market_buy_order["info"], indent=4)))
-    except Exception as e:
-        line_notify(str(e))
+    while not check:
+        try:
+            market_buy_order = binance.create_market_buy_order("BTC/USDT", order_amount)
+            check = True
+            line_notify(str(json.dumps(market_buy_order["info"], indent=4)))
+        except Exception as e:
+            check = False
+            time.sleep(10)
+            line_notify(str(e))
 
+    if check:
+        usdt_balance = binance.fetch_balance()["USDT"]["free"]
+        btc_balance = binance.fetch_balance()["BTC"]["free"]
+        avg_cost = round((1000 - usdt_balance) / btc_balance, 3)
+        return_balance = (usdt_balance + btc_balance * btc_price - 1000) * 100 / 1000
+        Estimated_Value = usdt_balance + btc_balance * btc_price
 
-    # 動作テスト用の指値注文の例
-    # try:
-    #     sample_price = 7333.02
-    #     order_amount_sample = round(order_size / sample_price, digit)
-    #     limit_order = binance.create_limit_buy_order("BTC/USDT", order_amount_sample, sample_price)
-    #     # print(json.dumps(limit_order, indent=4))
-    #     line_notify(str(json.dumps(limit_order["info"], indent=4)))
-    # except Exception as e:
-    #     line_notify(str(e))
-
-    usdt_balance = binance.fetch_balance()["USDT"]["free"]
-    btc_balance = binance.fetch_balance()["BTC"]["free"]
-    avg_cost = round((1000 - usdt_balance) / btc_balance, 3)
-    return_balance = (usdt_balance + btc_balance * btc_price - 1000) * 100 / 1000
-    Estimated_Value = usdt_balance + btc_balance * btc_price
-
-    text_1 = "BTC/USDT: " + str(btc_price) + "\n" + "\n"
-    text_2 = "[Balance] \n" + "USDT: " + str(round(usdt_balance, 2)) + "\n" + "BTC: " + str(
-        round(btc_balance, 8)) + "\n" + "Avg: " + str(avg_cost) + "\n" + "\n"
-    text_3 = "Estimated Value: " + str(round(Estimated_Value, 3)) + "\n"
-    text_4 = "Return: " + str(round(return_balance, 3)) + "%"
-    line_notify(text_1 + text_2 + text_3 + text_4)
-
+        text_1 = "BTC/USDT: " + str(btc_price) + "\n" + "\n"
+        text_2 = "[Balance] \n" + "USDT: " + str(round(usdt_balance, 2)) + "\n" + "BTC: " + str(
+            round(btc_balance, 8)) + "\n" + "Avg: " + str(avg_cost) + "\n" + "\n"
+        text_3 = "Estimated Value: " + str(round(Estimated_Value, 3)) + "\n"
+        text_4 = "Return: " + str(round(return_balance, 3)) + "%"
+        line_notify(text_1 + text_2 + text_3 + text_4)
+    else:
+        line_notify("EROOOOOR!!!")
 
 # if __name__ == '__main__':
 #     lambda_handler(None, None)
